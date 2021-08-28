c06fbd390a8e738c4e7ad9a7c5a0a248d3371223
Chris Regan <chrisreg007@gmail.com>
2021-08-28 15:51:17 -0400

    graphql setup started

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from } from '@apollo/client';
+import {onError} from '@apollo/client/link/error';
 import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Wrapper from './components/Wrapper';
 import NowWhat from './components/NowWhat';
+
+
+const errorLink = onError(({graphQLErrors, networkError})=>{
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
+    );
+  }
+})
+const link = from([
+  errorLink,
+  new HttpLink({uri: "https://react.eogresources.com/graphql"})
+])
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+})
 
 const theme = createTheme({
   palette: {
